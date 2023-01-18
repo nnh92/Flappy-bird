dafd18f78f340248e420fe4abd59b26d261c087f
nnh92.vn@gmail.com <nnh92.vn@gmail.com>
2023-01-18 21:15:08 +0900

    18012023-1

--- Flappy bird/Flappy_bird.py
+++ Flappy bird/Flappy_bird.py
@@ -3,15 +3,17 @@ import pygame
 class Bird:
     def __init__(self):
         pygame.init()
-        self.xScreen, self.yScreen = 400,600
+        self.xScreen, self.yScreen = 500,680
         linkBackground = './Data/Background.jpg'
         self.screen = pygame.display.set_mode((self.xScreen, self.yScreen))
 
         pygame.display.set_caption('Flappy bird!')
         self.background = pygame.image.load(linkBackground)
-        
-
+        linkIcon = pygame.image.load('./Data/Bird.png')
+        pygame.display.set_icon(linkIcon)
         self.gamerunning = True
+
+        self.Floor = pygame.image.load('./Data/Floor3.png')
         
         
 
@@ -19,10 +21,13 @@ class Bird:
         while self.gamerunning:
             for event in pygame.event.get():
                 self.screen.blit(self.background,(0,-500))
+                self.screen.blit(self.Floor,(0,500))
                 if event.type == pygame.QUIT:
                     self.gamerunning = False
 
-                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        print(1)
 
             pygame.display.flip()
             pygame.display.update()
