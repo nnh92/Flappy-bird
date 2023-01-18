c7bd7418740f96637210c7115b71ca8458ed2f38
nnh92.vn@gmail.com <nnh92.vn@gmail.com>
2023-01-18 22:04:20 +0900

    18012023-2

--- Flappy bird/Flappy_bird.py
+++ Flappy bird/Flappy_bird.py
@@ -3,25 +3,34 @@ import pygame
 class Bird:
     def __init__(self):
         pygame.init()
-        self.xScreen, self.yScreen = 500,680
-        linkBackground = './Data/Background.jpg'
+        self.xScreen, self.yScreen = 336,550
+        linkBackground = './Data/background_night.png'
         self.screen = pygame.display.set_mode((self.xScreen, self.yScreen))
 
         pygame.display.set_caption('Flappy bird!')
-        self.background = pygame.image.load(linkBackground)
-        linkIcon = pygame.image.load('./Data/Bird.png')
+        self.background = pygame.image.load(linkBackground).convert()
+        self.background = pygame.transform.scale(self.background,(self.xScreen, self.yScreen))
+        linkIcon = pygame.image.load('./Data/Bird.png').convert()
         pygame.display.set_icon(linkIcon)
         self.gamerunning = True
 
-        self.Floor = pygame.image.load('./Data/Floor3.png')
+        self.Floor = pygame.image.load('./Data/Floor.png').convert()
+
+        self.BirdImg = pygame.image.load('./Data/yellowbird-midflap.png').convert()
+        self.Bird = pygame.transform.scale(self.BirdImg, (50,50))
+        self.Bird_x_Pos, self.Bird_y_Pos = 40, self.yScreen/2
         
-        
+        self.Clock = pygame.time.Clock()
+        self.Floor_x_Pos = 0
+
+    def Draw_Floor(self):
+        self.screen.blit(self.Floor,(self.Floor_x_Pos,450))
+        self.screen.blit(self.Floor,(self.Floor_x_Pos+self.xScreen,450))
 
     def run(self):
         while self.gamerunning:
             for event in pygame.event.get():
-                self.screen.blit(self.background,(0,-500))
-                self.screen.blit(self.Floor,(0,500))
+
                 if event.type == pygame.QUIT:
                     self.gamerunning = False
 
@@ -29,11 +38,20 @@ class Bird:
                     if event.key == pygame.K_SPACE:
                         print(1)
 
+            self.screen.blit(self.background,(0,0))
+            self.screen.blit(self.Bird, (self.Bird_x_Pos, self.Bird_y_Pos))
+
+            # Ve ground
+            self.Floor_x_Pos -= 2
+            self.Draw_Floor()
+            if self.Floor_x_Pos <= -self.xScreen:
+                self.Floor_x_Pos = 0
+            
             pygame.display.flip()
             pygame.display.update()
+            self.Clock.tick(60)
 
         pygame.quit()
-
 
 if __name__ == '__main__':
     bird = Bird()
