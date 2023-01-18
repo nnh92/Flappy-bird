dfc929904cf14a4dc2b5581af402bb0b43abaf10
nnh92.vn@gmail.com <nnh92.vn@gmail.com>
2023-01-18 19:40:02 +0900

    1

--- Flappy bird/Flappy_bird.py
+++ Flappy bird/Flappy_bird.py
@@ -3,9 +3,31 @@ import pygame
 class Bird:
     def __init__(self):
         pygame.init()
+        self.xScreen, self.yScreen = 400,600
+        linkBackground = './Data/Background.jpg'
+        self.screen = pygame.display.set_mode((self.xScreen, self.yScreen))
+
+        pygame.display.set_caption('Flappy bird!')
+        self.background = pygame.image.load(linkBackground)
+        
+
+        self.gamerunning = True
+        
+        
 
     def run(self):
-        pass
+        while self.gamerunning:
+            for event in pygame.event.get():
+                self.screen.blit(self.background,(0,-500))
+                if event.type == pygame.QUIT:
+                    self.gamerunning = False
+
+                
+
+            pygame.display.flip()
+            pygame.display.update()
+
+        pygame.quit()
 
 
 if __name__ == '__main__':
