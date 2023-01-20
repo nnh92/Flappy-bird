978996fb805a8ca0aee2cc4ec729fcf6271da4c2
nnh92.vn@gmail.com <nnh92.vn@gmail.com>
2023-01-20 23:28:34 +0900

    20012023-1

--- Flappy bird/Flappy_bird.py
+++ Flappy bird/Flappy_bird.py
@@ -20,7 +20,11 @@ class Bird:
         self.Clock = pygame.time.Clock()
         self.Floor_x_Pos = 0
         self.Col_x_pos = 0
-        self.Col_y_pos = 100
+        self.Col_B_Height = 100
+        self.distance = 150
+        self.Col_Height = 500
+        self.FPS = 60
+        self.Score = 0
         #Thong so Floor
         self.Floor = pygame.image.load('./Data/Floor.png').convert()
         #Thong so bird
@@ -28,19 +32,18 @@ class Bird:
         self.Bird = pygame.transform.scale(self.BirdImg, (50,50))
         self.Bird_x_Pos, self.Bird_y_Pos = 40, self.yScreen/2
         #Thong so Column
-        self.Col_Img = pygame.image.load('./Data/tube2.png').convert()
-        
+        self.Col_Img_Bot = pygame.image.load('./Data/tube2.png').convert()
+        self.Col_Img_Bot = pygame.transform.scale(self.Col_Img_Bot, (45,self.Col_Height))
+        self.Col_Img_Top = pygame.image.load('./Data/tube1.png').convert()
+        self.Col_Img_Top = pygame.transform.scale(self.Col_Img_Top, (45,self.Col_Height))
     def Bird_Move(self):
         self.Move += 0.5 * self.gravity
         self.bird_rect.centery += self.Move
         self.screen.blit(self.Bird, self.bird_rect)
 
-    def Draw_Column(self, pos):
-        self.screen.blit(self.Col_Img, pos)
-
     def Draw_Floor(self):
-        self.screen.blit(self.Floor,(self.Floor_x_Pos,450))
-        self.screen.blit(self.Floor,(self.Floor_x_Pos+self.xScreen,450))
+        self.screen.blit(self.Floor,(self.Floor_x_Pos,500))
+        self.screen.blit(self.Floor,(self.Floor_x_Pos+self.xScreen,500))
 
     def run(self):
         self.bird_rect = self.Bird.get_rect(center = (50,self.yScreen/2))
@@ -52,7 +55,7 @@ class Bird:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         self.Move = 0
-                        self.Move -= 4
+                        self.Move -= 2.5
 
             self.screen.blit(self.background,(0,0))
             self.Bird_Move()
@@ -60,10 +63,15 @@ class Bird:
             self.Col_x_pos -= 2
             
             if self.Col_x_pos <= -self.xScreen-52:
-                self.Col_y_pos = random.randrange(50,300,25)
+                self.Col_B_Height = random.randrange(100,self.yScreen - self.distance - 50,25)
                 self.Col_x_pos = 0
-            self.Col_Pos = (336+self.Col_x_pos, 150 + self.Col_y_pos)
-            self.Draw_Column(self.Col_Pos)
+                self.Score += 1
+                if self.Score % 10 == 0:
+                    self.FPS += 5
+            self.Col_Pos_B = (self.xScreen + self.Col_x_pos, self.yScreen - self.Col_B_Height)
+            self.Col_Pos_T = (self.xScreen + self.Col_x_pos, self.yScreen - self.Col_B_Height - self.distance - self.Col_Height)
+            self.screen.blit(self.Col_Img_Bot, self.Col_Pos_B)
+            self.screen.blit(self.Col_Img_Top, self.Col_Pos_T)
             # Ve ground
             self.Floor_x_Pos -= 2
             self.Draw_Floor()
@@ -71,7 +79,7 @@ class Bird:
                 self.Floor_x_Pos = 0
             #pygame.display.flip()
             pygame.display.update()
-            self.Clock.tick(120)
+            self.Clock.tick(self.FPS)
 
         pygame.quit()
 
